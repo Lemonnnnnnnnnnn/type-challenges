32304cc4cc7843c0a268f335614e875ec560c378
soul <1041707577@qq.com>
2024-01-03 09:54:29 +0800

    feat:  omit

--- playground/medium/00003-medium-omit.ts
+++ playground/medium/00003-medium-omit.ts
@@ -30,7 +30,15 @@
 
 /* _____________ Your Code Here _____________ */
 
-type MyOmit<T, K> = any
+type MyPick<T, K extends keyof T> = {
+  [P in K]: T[P]
+}
+
+type MyExclude<T, K> = T extends K ? never : T
+
+// Omit 是 Pick 的反方法，所以考虑将除了 K 以外的属性从 T 中 Pick 出来
+// 排除一部分属性, 考虑使用 Exclude 方法, Exclude 方法对于指定的属性返回 never, 这样 Pick<T , never> 就可以把我们不想要的属性筛选掉 
+type MyOmit<T, K extends keyof T> = MyPick<T, MyExclude<keyof T, K>>
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
