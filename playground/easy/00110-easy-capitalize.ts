611fc73bc35ff4179a6cb4711986dcfcfe97f590
soul <104170577@qq.com>
2024-01-19 23:24:57 +0800

    feat: captialize

--- playground/easy/00110-easy-capitalize.ts
+++ playground/easy/00110-easy-capitalize.ts
@@ -18,7 +18,7 @@
 
 /* _____________ Your Code Here _____________ */
 
-type MyCapitalize<S extends string> = any
+type MyCapitalize<S extends string> = S extends `${infer f}${infer rest}` ? `${Uppercase<f>}${rest}` : S
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
