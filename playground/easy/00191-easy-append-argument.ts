801c6602eead2b65388b2f105ef80527e343ad2d
soul <104170577@qq.com>
2024-01-26 20:59:12 +0800

    feat: append argument

--- playground/easy/00191-easy-append-argument.ts
+++ playground/easy/00191-easy-append-argument.ts
@@ -23,7 +23,7 @@
 
 /* _____________ Your Code Here _____________ */
 
-type AppendArgument<Fn, A> = any
+type AppendArgument<Fn extends (...rest: any[]) => any, A> = Fn extends (...rest: infer P) => infer R ? (...rest: [...P, A]) => R : never
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
